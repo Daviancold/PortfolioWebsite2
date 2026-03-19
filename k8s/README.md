0b9cfb7e789570c5a2309065784cd77a9ca6118f
Davian Kho <daviankho1@gmail.com>
2026-03-19 15:28:50 +0800

    feat: add cloudflare and standardise yaml naming

---
+++ k8s/README.md
@@ -0,0 +1,4 @@
+Before setting up, need to add cloudflared token secret to cluster. Refer to link for more info
+https://developers.cloudflare.com/cloudflare-one/networks/connectors/cloudflare-tunnel/deployment-guides/kubernetes/
+
+(Note: To consider upgrading to cloud vault storage and storing token there instead)
--- k8s/bootstrap/bootstrap.sh
+++ k8s/bootstrap/bootstrap.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # bootstrap.sh — run once after terraform apply
 
-CLUSTER_ID=$(terraform output -raw cluster_id)
-REGION=$(terraform output -raw region)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+K8S_DIR="$(dirname "$SCRIPT_DIR")"
 
-# Get kubeconfig
-oci ce cluster create-kubeconfig \
-  --cluster-id $CLUSTER_ID \
-  --file ~/.kube/config \
-  --region $REGION \
-  --token-version 2.0.0
+# CLUSTER_ID=$(terraform output -raw cluster_id)
+# REGION=$(terraform output -raw region)
+
+# # Get kubeconfig
+# oci ce cluster create-kubeconfig \
+#   --cluster-id $CLUSTER_ID \
+#   --file ~/.kube/config \
+#   --region $REGION \
+#   --token-version 2.0.0
 
 # Install ArgoCD
-kubectl create namespace argocd
+kubectl get namespace argocd &>/dev/null || kubectl create namespace argocd
 kubectl apply -n argocd -f \
   https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
@@ -20,4 +23,8 @@ kubectl apply -n argocd -f \
 kubectl wait --for=condition=available deployment/argocd-server \
   -n argocd --timeout=180s
 
-kubectl apply -f k8s/argocd/apps.yaml
+# Apply secrets before app deployment
+kubectl get secret cloudflare-tunnel-token &>/dev/null || \
+  kubectl create -f "$K8S_DIR/apps/cloudflared/secret.yaml"
+
+kubectl apply -f "$K8S_DIR/bootstrap/root-app.yaml"
