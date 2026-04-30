a25504e6a4c7c4183838ee05e9e1a1663b0527ee
Davian Kho <daviankho1@gmail.com>
2026-04-30 15:21:11 +0800

    git commit -m "feat: update toolkit with minecraft maintenance commands"

--- k8s/bootstrap/toolkit.sh
+++ k8s/bootstrap/toolkit.sh
@@ -1,7 +1,20 @@
-# For entering cluster to run networking command and testing
+### For entering cluster to run networking command and testing ###
 kubectl run debug --image=curlimages/curl --rm -it --restart=Never -- /bin/sh
 
-# To change grafana password
-kubectl patch secret prometheus-stack-grafana -n monitoring \
-  --type merge \
-  -p "{\"data\": {\"admin-password\": \"$(echo -n 'yournewpassword' | base64)\"}}"
+
+
+### For minecraft server data management ###
+kubectl scale statefulset minecraft-server -n apps --replicas=0
+
+### Copies your local folder into the /data directory on the Block Volume
+kubectl cp ./local-world-folder/ apps/$(kubectl get pod -l app=minecraft-admin -n apps -o jsonpath='{.items[0].metadata.name}'):/data/
+### Pulls the world from the cloud to your laptop
+kubectl cp apps/$(kubectl get pod -l app=minecraft-admin -n apps -o jsonpath='{.items[0].metadata.name}'):/data ./backup-$(date +%F)
+
+### Enter the interactive shell
+kubectl exec -it deployment/minecraft-admin -n apps -- bash
+chown -R 1000:1000 /data
+
+### Helpful world data edit commands
+kubectl exec -it deployment/minecraft-admin -n apps -- nano /data/fabric/whitelist.json
+# nano /data/server.properties
